11d4a4826371ce3ae650d113eb57ac6dbb9bb356
Charles <charlesowino180@gmail.com>
2023-08-11 15:27:21 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry function
+  *
+  *Return: always 0 success
+  */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
