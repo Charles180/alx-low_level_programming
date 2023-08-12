7638ff0a8703f4e7f00de0ee19aa4d5b9a74db09
Charles <charlesowino180@gmail.com>
2023-08-11 17:15:52 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,17 @@ int main(void)
 {
 	int number1, number2;
 
-	for (number1 = 0; number1 < 10; number1++)
+	for (number1 = 0; number1 < 8; number1++)
 	{
-		for (number2 = 0; number2 < 10; number2++)
+		for (number2 = number1 + 1; number2 <= 9; number2++)
 		{
-			putchar((number1 % 10) + '0');
-			putchar((number2 % 10) + '0');
-			if (number1 == 9 && number2 == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
+			putchar(number1 + '0');
+			putchar(number2 + '0');
+			if (number1 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	printf("\n");
