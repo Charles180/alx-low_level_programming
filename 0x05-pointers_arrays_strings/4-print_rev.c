684401196fd479ac20d17e7d8cb5346e32dcf317
Charles <charlesowino180@gmail.com>
2023-08-22 02:27:31 +0300

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * print_revs - function to print strings followed in reverse
+ *
+ * @s: Pointer for string to be printed
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int k;
+
+	for (k = _strlen(s) - 1; k >= 0; k--)
+	{
+		_putchar(*(s + k));
+	}
+	_putchar('\n');
+}
