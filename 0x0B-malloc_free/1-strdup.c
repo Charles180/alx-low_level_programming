1473f6cb42d5e0e02b011794042dfb9734c760f5
Charles <charlesowino180@gmail.com>
2023-09-06 02:01:37 +0300

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,33 +4,23 @@
 /**
   *_strdup - The function to return the required pointer
   *
-  *@s: The string
+  *@str: The string
   *
   *Return: Null if *s is NULL
   */
-char *_strdup(char *s)
+char *_strdup(char *str)
 {
-	int i, size;
-	char *a;
+	char *duplicate;
 
-	if (s == NULL)
+	duplicate = malloc(strlen(str) + 1);
+	if (duplicate == NULL)
 	{
 		return (NULL);
 	}
-	for (size = 0; s[size] != '\0'; size++)
+	if (str == NULL)
 	{
-		a = malloc(size * sizeof(*s) + 1);
-		if (a == 0)
-		{
-			return (NULL);
-		}
-		else
-		{
-			for (i = 0; i <= size; i++)
-			{
-				a[i] = s[i];
-			}
-		}
+		return (NULL);
 	}
-	return (a);
+	strcpy(duplicate, str);
+	return (duplicate);
 }
