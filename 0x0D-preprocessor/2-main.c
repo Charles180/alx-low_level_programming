788a0b6d6d49bfb16ed23c573b9acc6bd3b4516b
Charles <charlesowino180@gmail.com>
2023-09-11 16:14:27 +0300

    prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - Entry point
+  *
+  *Return: O success
+  */
+int main(void)
+{
+	printf("The program is compiled from the file: %s\n", __FILE__);
+	return (0);
+}
+
