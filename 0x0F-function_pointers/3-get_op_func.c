c6b39e0631618c838838bfa0f384faa5d49e7342
Charles <charlesowino180@gmail.com>
2023-09-14 01:54:44 +0300

    function that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <string.h>
+/**
+  *get_op_func - selects the correct funtion to perform user operation
+  *
+  *@s: operation to perform
+  *
+  *Return: pointer to the correct function
+  */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; ops[i].op != NULL; i++)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			break;
+	}
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+  *main - Function that performs simple operations
+  *
+  *@argc: arguments count
+  *
+  *@argv: argument vector
+  *
+  *Return: 0 success
+  */
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
+	int num1, num2, a;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = func(num1, num2);
+	printf("%d\n", a);
+	return (0);
+}
