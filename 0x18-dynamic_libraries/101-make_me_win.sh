ba2da8da87032a38d4fdbaef88b196352eb6b004
Charles <charlesowino180@gmail.com>
2023-11-14 00:53:17 +0300

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Charles180/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -P .. https://raw.githubusercontent.com/Charles180/alx-low_level_programming/master/0x18-dynamic_libraries/test.so
+export LD_PRELOAD="$PWD/../test.so"
